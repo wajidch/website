d9e9223ab2de8f7597f6194bda674a1609ed33c8
wajid <wajid.javeed@appflox.com>
2019-10-25 17:56:08 +0500

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'http://157.245.40.90:8080/api/',
+  apiUrl:'https://api.appflox.com:446/api/',
   siteKey:'6Ldpb78UAAAAAN9-wyg2jWKEDlxXnyOqpoK31X6J'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://157.245.40.90:8080/api/',
+  apiUrl:'https://api.appflox.com:446/api/',
   siteKey:'6Ldpb78UAAAAAN9-wyg2jWKEDlxXnyOqpoK31X6J'
 };
 
